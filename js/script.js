e47ebd7f2c798f1b6ecdd7cefff498b05f859b03
karin valcanover <valcanoverkarin@outlook.it>
2021-04-27 16:48:35 +0200

    stampo l' email l' apposita API di Boolean

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
+//1. Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail 
+//   generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
 var app = new Vue (
     {
         el: '#root',
         data: {
+            //1a. creo array di e mail
+            eMails: []
             
         },
         methods () {
 
         },
         mounted() {
+
+                //1c. faccio la richiesta
+                axios
+                    //1d. aggiungo l' apposita API di Boolean
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    //1e. risposta del server
+                    .then( (response) => {
+                        //1f. salvo il risultato nella variabile
+                        const result = response.data;
+                        //1g aggiungo il risultatol'email
+                        this.eMails.push(result.response);
+                });
+            
 
         }
     }
